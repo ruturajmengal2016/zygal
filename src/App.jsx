04ef75f467e640720ff0c861ab0006f61b860730
Ruturaj <ruturajmengal2016@gmail.com>
2023-07-12 15:45:52 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Canvas from "./canvas/Canvas";
+function App() {
+  return (
+    <div className="App">
+      <Canvas width={700} height={500} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/canvas/Canvas.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useOnDraw } from "./hooks";
+
+const Canvas = ({ width, height }) => {
+  const [url, setUrl] = useState("");
+  const { setCanvasRef, onCanvasMouseDown } = useOnDraw(onDraw);
+
+  function onDraw(ctx, point, prevPoint) {
+    drawLine(prevPoint, point, ctx, "#FFC0CB", 5);
+  }
+  const download = () => {
+    const url = document.getElementById("canvas").toDataURL("image/jpg");
+    setUrl(url);
+  };
+  function drawLine(start, end, ctx, color, width) {
+    start = start ?? end;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  return (
+    <>
+      <canvas
+        id="canvas"
+        width={width}
+        height={height}
+        onMouseDown={onCanvasMouseDown}
+        style={canvasStyle}
+        ref={setCanvasRef}
+      />
+      <a href={url} download="image.txt" onClick={download}>
+        Download
+      </a>
+    </>
+  );
+};
+
+export default Canvas;
+
+const canvasStyle = {
+  border: "1px solid black",
+};
